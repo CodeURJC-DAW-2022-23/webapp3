f0945a08030bb45fe057aa688fbf669d67d9b0dd
Shadith <s.perezr@uniandes.edu.co>
2023-03-08 23:12:05 +0100

    director

--- backend/movieframe/src/main/java/es/webapp3/movieframe/controller/DirectorController.java
+++ backend/movieframe/src/main/java/es/webapp3/movieframe/controller/DirectorController.java
@@ -3,7 +3,9 @@ package es.webapp3.movieframe.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,3 +23,12 @@ public class DirectorController {
     public List<Director> getAllDirectors() {
         return directorRepository.findByName();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Director> obtenerDirectorPorId(@PathVariable(value = "id") Long directorId) {
+        Director director = directorRepository.findById(directorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Director no encontrado con el ID: " + directorId));
+        return ResponseEntity.ok().body(director);
+    }
+
+}
