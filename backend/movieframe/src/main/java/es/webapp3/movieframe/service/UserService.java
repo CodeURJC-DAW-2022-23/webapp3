868c694edbd11c7c8afa7f74f19d16fad5eb20d1
Shadith <s.perezr@uniandes.edu.co>
2023-03-11 14:35:50 +0100

    Error en user

--- backend/movieframe/src/main/java/es/webapp3/movieframe/service/UserService.java
+++ backend/movieframe/src/main/java/es/webapp3/movieframe/service/UserService.java
@@ -12,33 +12,33 @@ import es.webapp3.movieframe.repository.UserRepository;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
 
-    public User findById(long id) {
-        return userRepository.findById(id)
-                .orElseThrow();
-    }
+	public User findById(long id) {
+		return userRepository.findById(id)
+				.orElseThrow();
+	}
 
-    public User saveOrUpdateUser(User user) {
-        return userRepository.save(user);
-    }
+	public User saveOrUpdateUser(User user) {
+		return userRepository.save(user);
+	}
 
-    public void deleteUser(long id) {
-        userRepository.deleteById(id);
-    }
+	public void deleteUser(long id) {
+		userRepository.deleteById(id);
+	}
 
-    // Login
-    public User authenticateUser(String username, String password) {
-        Optional<User> user = userRepository.findByUsername(username);
-        if (user.isPresent() && user.get().getPassword().equals(password)) {
-            return user.get();
-        } else {
-            return null;
-        }
-    }
+	// Login
+	public User authenticateUser(String username, String password) {
+		Optional<User> user = userRepository.findByUsername(username);
+		if (user.isPresent() && user.get().getPassword().equals(password)) {
+			return user.get();
+		} else {
+			return null;
+		}
+	}
 }
