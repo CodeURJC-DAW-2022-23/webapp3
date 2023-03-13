a27c918536c07bb86af6f49e260cf29c2a739cee
Javarto <54403354+Javarto@users.noreply.github.com>
2023-03-13 12:30:37 +0100

    Añadir persistencia al hacer login y que se muestre en el header de manera correcta

--- backend/movieframe/src/main/java/es/webapp3/movieframe/controller/Home.java
+++ backend/movieframe/src/main/java/es/webapp3/movieframe/controller/Home.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import es.webapp3.movieframe.model.User;
 import es.webapp3.movieframe.service.MovieService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class Home {
@@ -15,8 +18,15 @@ public class Home {
     private MovieService movieService;
     
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, HttpSession session){
 
+        // Check if session attribute exists
+        if (session.getAttribute("loggedInUser") != null) {
+            User loggedInUser = (User) session.getAttribute("loggedInUser");
+            model.addAttribute("loggedInUser", loggedInUser);
+        }
+        
+        System.out.println("Flash attribute not found");
         model.addAttribute("movies", movieService.findAll());
         return "initial_screen";
     }
